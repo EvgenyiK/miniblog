4804ec2953e5bbe8c3df9d038b53bb86985f393b
evgen <evgenprogrammer@gmail.com>
2022-07-07 18:46:01 +0300

    add Reader class

--- includes/classes/BlogReader.php
+++ includes/classes/BlogReader.php
@@ -9,11 +9,27 @@ class BlogReader{
     protected $type;
 
     public function __construct(){
-        
         $this->db = new Database();
         $this->type = BlogReader::READER;
     }    
     
-    //Add getPostsFromDB() here
+    public function getPostsFromDB(){
+        $sql = "SELECT id, unix_timestamp(post_date) as
+        `post_date`, username, title, post, audience FROM
+        posts WHERE audience <= :audience ORDER BY id DESC";
+
+        $values = array(
+            array(':audience', $this->type)
+        );
+
+        $result = $this->db->queryDB($sql,
+            Database::SELECTALL, $values);
+
+        if (count($result) == 0){
+            return false;
+        }else{
+            return $result;
+        }
+    }
     
 }
