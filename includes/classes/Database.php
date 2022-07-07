57d683290c74fdb90b501937f999731236bb495e
evgen <evgenprogrammer@gmail.com>
2022-07-07 18:23:28 +0300

    sdd DB class

--- includes/classes/Database.php
+++ includes/classes/Database.php
@@ -10,12 +10,29 @@ class Database{
 
     public function __construct(){
         
-        $pdo = new PDO("mysql:host=mysql-server;dbname=project", "root", "secret");
-        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); 
+        $this->pdo = new PDO("mysql:host=mysql-server;dbname=project", "root", "secret");
+        $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         
     }
 
-    //Add queryDB() here
-    
+    public function queryDB($sql,$mode,$values = []){
+        $stmt = $this->pdo->prepare($sql);
+
+        foreach($values as $valueToBind){
+            $stmt->bindValue($valueToBind[0], $valueToBind[1]);
+        }
+
+        $stmt->execute();
+
+        if ($mode!= Database::SELECTSINGLE && $mode!= Database::SELECTALL && $mode!= Database::EXECUTE){
+            throw new Exception('Invalid Mode');
+        }elseif ($mode == Database::SELECTSINGLE){
+            return $stmt->fetch(PDO::FETCH_ASSOC);
+        }elseif ($mode == Database::SELECTALL){
+            return $stmt->fetchAll(PDO::FETCH_ASSOC);
+        }
+
+    }
+
 }
     
