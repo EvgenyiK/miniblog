f37228cb4166fac61f4bb14dfb5a97650d0c70f3
evgen <evgenprogrammer@gmail.com>
2022-07-08 12:57:00 +0300

    add classes and begin processors

--- includes/process/p-write.php
+++ includes/process/p-write.php
@@ -1,2 +1,26 @@
 <?php
 
+if (isset($_SESSION['username'],$_SESSION['is_admin'])){
+    header("Location: write.php");
+}else{
+    $h = new Helper();
+    $msg = '';
+    $post = '';
+    $title = '';
+    $audience = '';
+
+    if (isset($_POST['submit'])){
+        $post = $_POST['post'];
+        $title = $_POST['title'];
+        $audience = $_POST['audience'];
+
+        if ($h->isEmpty(array($title, $post, $audience)))
+            $msg = 'All fields are required';
+        else{
+            $admin = new Admin($_SESSION['username']);
+            $admin->insertIntoPostDB($title, $post, $audience);
+            $msg = 'Message saved successfully';
+        }
+    }
+}
+
